2f08fa9495f906f69871477c04a08439b52ba8df
Dana <dana.lalovaod@gmail.com>
2023-03-11 17:30:31 +0200

    feat: 1 task finished

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+
 //Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
 // console.log(galleryItems);
 const galleryRef = document.querySelector(".gallery");
@@ -25,6 +26,15 @@ galleryRef.insertAdjacentHTML("afterbegin", markUp);
 //Реализация делегирования на div.gallery и получение url большого изображения.
 galleryRef.addEventListener("click", onGalleryClickHandler);
 
+
+
+// Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект
+//  ссылки на минифицированные (.min) файлы библиотеки.
+
+// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+
+// Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна
+// с изображением из примеров библиотеки basicLightbox.
 function onGalleryClickHandler(event) {
   event.preventDefault();
 
@@ -34,12 +44,11 @@ function onGalleryClickHandler(event) {
 
   const bigImageLink = event.target.dataset.source;
   console.log("click", bigImageLink);
+
+  const instance = basicLightbox.create(
+    `<img src="${bigImageLink}" width="800" height="600">`
+  );
+  return instance.show();
 }
 
-// Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект
-//  ссылки на минифицированные (.min) файлы библиотеки.
 
-// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
-
-// Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна
-// с изображением из примеров библиотеки basicLightbox.
